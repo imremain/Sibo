a8744f36ce5fa3cbdbb385bde85724270a285665
Xerox <alfredo.montoya@cesunbc.edu.mx>
2020-11-09 11:34:38 -0800

    Initial Commit

---
+++ js/init.js
@@ -0,0 +1,7 @@
+(function($){
+  $(function(){
+    $('.dropdown-trigger').dropdown();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
